eb9550d832a13a03b6f8626276e0449a96260bc0
shambhavik14 <68625339+shambhavik14@users.noreply.github.com>
2023-01-27 21:49:46 -0500

    Uploading my code
    Uploading my code

--- Assignment2_DataStructure/part1/src/Deque.d
+++ Assignment2_DataStructure/part1/src/Deque.d
@@ -4,38 +4,31 @@
 // This will execute each of the unit tests telling you if they passed.
 
 import std.stdio;
+import std.algorithm : moveAll;
 
-/*
-    The following is an interface for a Deque data structure.
-    Generally speaking we call these containers.
-    
-    Observe how this interface is a templated (i.e. Container(T)),
-    where 'T' is a placeholder for a data type.
-*/
 interface Container(T){
-    // Element is on the front of collection
-    void push_front(T x);
-    // Element is on the back of the collection
-    void push_back(T x);
-    // Element is removed from front and returned
-    // assert size > 0 before operation
-    T pop_front();
-    // Element is removed from back and returned
-    // assert size > 0 before operation
-    T pop_back();
-    // Retrieve reference to element at position at index
-    // assert pos is between [0 .. $] and size > 0
-    ref T at(size_t pos);
-    // Retrieve reference to element at back of position
-    // assert size > 0 before operation
-    ref T back();
-    // Retrieve element at front of position
-    // assert size > 0 before operation
-    ref T front();
-    // Retrieve number of elements currently in container
-    size_t size();
+	// Element is on the front of collection
+	void push_front(T x);
+	// Element is on the back of the collection
+	void push_back(T x);
+	// Element is removed from front and returned
+	// assert size > 0 before operation
+	T pop_front();
+	// Element is removed from back and returned
+	// assert size > 0 before operation
+	T pop_back();
+	// Retrieve reference to element at position at index
+	// assert pos is between [0 .. $] and size > 0
+	ref T at(size_t pos);
+	// Retrieve reference to element at back of position
+	// assert size > 0 before operation
+	ref T back();
+	// Retrieve element at front of position
+	// assert size > 0 before operation
+	ref T front();
+        // Retrieve number of elements currently in container
+        size_t size();
 }
-
 /*
     A Deque is a double-ended queue in which we can push and
     pop elements.
@@ -44,23 +37,157 @@ interface Container(T){
           a struct depending on how we want to extend or use it.
           Either is fine for this assignment.
 */
+
 class Deque(T) : Container!(T){
-    // Implement here
+	// Implement here
+	T[] arr;
+	          
+	override void push_front(T x) {
+
+		arr = [x] ~ arr;
+	}
+		      
+	override void push_back(T x) {
+		arr ~= x;
+	}
+		          
+	override T pop_front() {
+			        
+	        auto sizet = size();
+		assert(sizet > cast(size_t) 0, "Deque is empty");
+		T x = arr[0];
+		moveAll(arr[1..$], arr[0..$-1]);
+		--sizet;
+		return x;
+						            
+	}
+			      
+	override T pop_back() {
+	
+		auto sizet = size();
+		assert(sizet > cast(size_t) 0, "Deque is empty");
+		T x = arr[$-1];
+		--sizet;
+		return x;
+	}
+			          
+	override ref T at(size_t pos) {
+					        
+		auto sizet = size();
+		assert(sizet > cast(size_t) 0, "Deque is empty");
+		assert(pos <= sizet && pos >= cast(size_t) 0, "This index does not exist");
+		return arr[pos];
+	}
+				      
+	override ref T front() {
+					            
+		auto sizet = size();
+		assert(sizet > cast(size_t) 0, "Deque is empty");
+		return arr[0];
+								      
+	}
+	
+	override ref T back() {
+						        
+		auto sizet = size(); 
+		assert(sizet > cast(size_t) 0, "Deque is empty");
+		return arr[$-1];
+	}
+					      
+	override size_t size() {
+		return arr.length;
+	}
 }
 
 // An example unit test that you may consider.
 // Try writing more unit tests in separate blocks
 // and use different data types.
-unittest{
-    auto myDeque = new Deque!(int);
-    myDeque.push_front(1);
-    auto element = myDeque.pop_front();
+unittest
+{
+	auto myDeque = new Deque!(int);
+	myDeque.push_front(1);
+	auto element = myDeque.pop_front();
     assert(element == 1);
 }
 
-void main(){
-    // No need for a 'main', use the unit test feature.
-    // Note: The D Compiler can generate a 'main' for us automatically
-    //       if we are just unit testing, and we'll look at that feature
-    //       later on in the course.	
+unittest
+{
+	auto myDeque = new Deque!(int);
+	myDeque.push_back(10);
+	auto element = myDeque.pop_back();
+	assert(element == 10);
+}
+
+unittest
+{
+	auto myDeque = new Deque!(int);
+    myDeque.push_front(1);
+	auto element = myDeque.front();
+	assert(element == 1);
+}
+
+unittest
+{
+	auto myDeque = new Deque!(int);
+    myDeque.push_back(10);
+	auto element = myDeque.back();
+	assert(element == 10);
+}
+
+unittest
+{
+	auto myDeque = new Deque!(int);
+    myDeque.push_back(10);
+	myDeque.push_front(1);
+    auto start = myDeque.front();
+	auto element = myDeque.back();
+	assert(start == 1);
+	assert(element == 10);
+}
+
+unittest
+{
+	auto myDeque = new Deque!(string);
+	myDeque.push_back("hey");
+	auto element = myDeque.pop_back();
+	assert(element == "hey");
+}
+
+unittest
+{
+	auto myDeque = new Deque!(string);
+    myDeque.push_front("hey");
+	auto element = myDeque.at(0);
+	assert(element == "hey");
+}
+
+unittest
+{
+	auto myDeque = new Deque!(int);
+    myDeque.push_back(10);
+	auto element = myDeque.at(0);
+	assert(element == 10);
+}
+
+unittest
+{
+	auto myDeque = new Deque!(int);
+    myDeque.push_back(9);
+	myDeque.push_front(3);
+    myDeque.push_front(1);
+	auto element = myDeque.at(1);
+    assert(element == 3);
+}
+
+unittest 
+{
+	auto myDeque = new Deque!(int);
+	myDeque.push_front(3);
+    auto element = myDeque.size();
+    assert(element == cast(size_t) 1);
+		  
+}
+
+void main() {
+
 }
