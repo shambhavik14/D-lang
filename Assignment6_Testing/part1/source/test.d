fe8ac827513ecd872e2c8b3bc1c0e4cc31362f42
DESKTOP-S7JEB9Q\shamb <kulkarni.sham@northeastern.edu>
2023-02-24 18:18:11 -0500

    adding change

--- Assignment6_Testing/part1/source/test.d
+++
@@ -1,6 +0,0 @@
-// ./source/test.d
-@("some other test in test.d")
-
-unittest{
-    assert(2==2,"2==2 should pass");
-}
--- Assignment6_Testing/part1/source/test2.d
+++
@@ -1,5 +0,0 @@
-// ./source/test2.d
-@("some other test in test2.d")
-unittest{
-    assert(3==3,"3==3 should pass");
-}
